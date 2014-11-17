9f7be3d68fd112804f3978f8305d9dc72d54e74a
Marcin Rudzki <mar.rudzki@gmail.com>
2014-11-17 20:28:16 +0100

    Add giveChange kata and reset wrapWords kata

---
+++ kata/giveChange/giveChange.js
@@ -0,0 +1,8 @@
+/**
+ * Napisz funkcję giveChange(sum), która zwróci daną sumę w postaci banknotów i monet.
+ * Na przykład giveChange(124.90) powinno zwrócić:
+ * { '100 PLN': 1, '20 PLN': 1, '2 PLN': 2, '50 gr': 1, '20 gr': 2 }
+ * Do dyspozycji mamy banknoty: '200 PLN', '100 PLN', '50 PLN', '20 PLN' i '10 PLN'
+ * oraz monety: '5 PLN', '2 PLN', '1 PLN',
+ * '50 gr', '20 gr', '10 gr', '5 gr', '2 gr' i '1 gr'
+ */
--- kata/wrapWords/wrapWords.js
+++ kata/wrapWords/wrapWords.js
@@ -1,32 +1,6 @@
-/* You write a single function named wrapWords() that takes two arguments: a string and a column number.
-   The function returns the string, but with line breaks inserted at just the right places
-   to make sure that no line is longer than the column number. You try to break lines at word boundaries.
-   Like a word processor, break the line by replacing the last space in a line with a newline.
- */
-
-function wrapWords( str, width ) {
-  var wrapped = [];
-  var slicePosition = 0;
-
-  if(str.length <= width) {
-    return str;
-  }
-  
-  do {
-    slicePosition = str.lastIndexOf(" ", width) + 1;
-    wrapped.push(str.slice(0, slicePosition));
-    str = str.slice(slicePosition);
-  } while (str.length >= width)
-
-  wrapped.push(str)
-
-  
-  return wrapped.join('\n')
-}
-
-/* Refactored version
-   
-   function wrapWords( str, width ) { 
-       return str.match( RegExp('.{1,' +width+ '}(\\s|$)' + '|\\S+?(\\s|$)', 'g') ).join( '\n' );
-   }
+/**
+ * You write a single function named wrapWords() that takes two arguments: a string and a column number.
+ * The function returns the string, but with line breaks inserted at just the right places
+ * to make sure that no line is longer than the column number. You try to break lines at word boundaries.
+ * Like a word processor, break the line by replacing the last space in a line with a newline.
  */
