c6b82f52633b38bf221cb52c98cac50868e9a196
Marcin Rudzki <mar.rudzki@gmail.com>
2014-11-17 22:27:32 +0100

    Adding capitals kata

---
+++ kata/capitals/capitals.js
@@ -0,0 +1,12 @@
+/**
+ * Write a method that takes an array of objects with two keys each: country or state, and capital.
+ * Keys may be symbols or strings.
+ * The method should return an array of sentences declaring the state or country and its capital.
+ *
+ * Examples:
+ * country_capitals = [{'country': 'Poland', 'capital': 'Warsaw'}]
+ * displayCapitals(country_capitals) // should return ["The capital of Poland is Warsaw"]
+ *
+ * mixed_capitals: [{"state": 'Texas', capital: 'Austin'}, {country: 'Spain', "capital": "Madrid"}]
+ * displayCapitals(mixed_capitals) // returns ["The capital of Texas is Austin", "The capital of Spain is Madrid"]
+ */
