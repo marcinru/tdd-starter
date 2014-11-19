9e1485140640a0ae3f9b45752d0cc392f6cd6fad
Marcin Rudzki <marcin.rudzki@roche.com>
2014-11-19 11:07:36 +0100

    Extending Greetings kata

--- kata/greetings/greetings.js
+++ kata/greetings/greetings.js
@@ -3,8 +3,16 @@
  * and then greets that name, capitalized
  * and ends with an exclamation mark.
  *
- * Examples:
+ * Basic version:
  * greet('John') // should return 'Hello John!'
  * greet('JACK') // should return 'Hello Jack!'
  * greet('maya') // should return 'Hello Maya!'
+ *
+ * Tip: If you know how, try to use String.prototype
+ *
+ * Extended version:
+ * greet('john smith') // should return 'Hello John Smith!'
+ * greet('JOHN F. KENNEDY') // returns 'Hello John F. Kennedy!'
+ *
+ * Tip: what's next? - http://ejohn.org/files/titleCaps.js
  */
